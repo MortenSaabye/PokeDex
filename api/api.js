7d2d1070b2554a3221bfdb910c9053d48c55e445
MortenSaabye <saabye93@gmail.com>
2017-07-16 12:04:14 +0200

    express to serve static assets

--- api/api.js
+++ api/api.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const port = process.ENV.PORT || 3001
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../build'));
+}
+
 const router = require('./routes')
 
 app.use('/api', router)
